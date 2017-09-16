7d3b08f81559e54f0507f86830fef19636f096cb
Nishant <princeofpbh@gmail.com>
2017-09-16 19:05:32 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
